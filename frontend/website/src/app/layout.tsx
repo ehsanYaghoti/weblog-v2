e228b6e707afdb9a4e827f816ec180eccbb98a94
Ehsan <ehsan.yaghoti@yahoo.com>
2026-07-16 16:58:16 +0330

    chore(dependency): install and init React Query

--- frontend/website/src/app/layout.tsx
+++ frontend/website/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Providers from "@/providers/providers";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -23,14 +24,18 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+
     return (
         <html
             lang="en"
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="min-h-full flex flex-col">
-                <main>{children}</main>
-                <Toaster />
+                <Providers>
+                    <main>{children}</main>
+                    <Toaster />
+                </Providers>
             </body>
 
         </html>
